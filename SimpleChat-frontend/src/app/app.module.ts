69980936d1a4494eca1be6885105300e3fca0d5b
unknown <merrill.huang@gmail.com>
2023-10-19 11:37:18 -0500

    generate main-page component

--- SimpleChat-frontend/src/app/app.module.ts
+++ SimpleChat-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
