ece964fa2854ccdba9051e53c867885de778757b
unknown <merrill.huang@gmail.com>
2023-10-19 11:42:30 -0500

    create route for '/rooms' that leads to main-page

--- SimpleChat-frontend/src/app/app-routing.module.ts
+++ SimpleChat-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
+  },
+  {
+    path: 'rooms',
+    component: MainPageComponent
   }
 ];
 
