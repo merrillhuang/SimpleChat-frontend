f59e89aa126c371d8cb185aa83929d16ff1332af
unknown <merrill.huang@gmail.com>
2023-10-20 10:42:58 -0500

    import RoomsService inside main-page

--- SimpleChat-frontend/src/app/main-page/main-page.component.ts
+++ SimpleChat-frontend/src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RoomsService } from '../services/rooms/rooms.service';
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent {
-
 }
