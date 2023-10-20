dcffb577885391d635ed4d766303daf36768cfb5
unknown <merrill.huang@gmail.com>
2023-10-20 14:03:55 -0500

    add functions for changing the page of results fetched from the API that main-page is displaying

--- SimpleChat-frontend/src/app/main-page/main-page.component.ts
+++ SimpleChat-frontend/src/app/main-page/main-page.component.ts
@@ -8,7 +8,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MainPageComponent implements OnInit {
 
-  rooms: any
+  currRooms: Object[]= [];
+  numPages: number = 1;
+  currPage: number = 1;
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +18,42 @@ export class MainPageComponent implements OnInit {
     this.http
     .get('http://api.open-notify.org/astros.json')
     .subscribe((response: any) => {
-      this.rooms = response;
+      const maxRooms = 2;
+      let length: number = response.people.length;
+      if (length % maxRooms !== 0) {
+        this.numPages = Math.floor(length / maxRooms) + 1;
+      } else {
+        this.numPages = length / maxRooms;
+      }
+      // if (length < maxRooms) {
+      //   for (let person of response.people) {
+      //     this.currRooms.push(person);
+      //   }
+      // } else {
+      //   if (this.numPages === this.currPage) {
+      //     for (let i = (maxRooms * (this.currPage-1)); i < response.people.length; i++) {
+      //       this.currRooms.push(response.people[i]);
+      //     }
+      //   }
+        // for (let i = n)
+        //   this.numPages = response.people.length / maxRooms;
+      // }
     });
   }
-}
+
+  changePage(pageNum: number) {
+    this.currPage = pageNum;
+  }
+
+  prevPage() {
+    if (this.currPage > 1) {
+      this.currPage--;
+    } 
+  }
+
+  nextPage() {
+    if (this.currPage < this.numPages) {
+      this.currPage++;
+    }
+  }
+ }
