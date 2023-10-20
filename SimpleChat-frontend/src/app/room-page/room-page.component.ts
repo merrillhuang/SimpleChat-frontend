2fe7b11b9d879d8a542f33369aa673416a5b1102
unknown <merrill.huang@gmail.com>
2023-10-20 15:46:05 -0500

    import Activated Route in room-page so it can access information from the URL

--- SimpleChat-frontend/src/app/room-page/room-page.component.ts
+++ SimpleChat-frontend/src/app/room-page/room-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-room-page',
